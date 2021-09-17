d192246c68a4a9b97e8a3a40d23b52df0df4a8df
Mark Andrew Miller <MAM@lbl.gov>
2021-09-17 16:42:48 -0400

    don't sort modified model

--- Makefile
+++ Makefile
@@ -8,8 +8,10 @@ clean:
 	rm -rf person_validation.txt
 
 personinfo.yaml:
-	curl https://raw.githubusercontent.com/linkml/linkml/main/examples/PersonSchema/personinfo.yaml > $@
-	# python add_prefixes_from_file.py
+	curl https://raw.githubusercontent.com/linkml/linkml/main/examples/PersonSchema/personinfo.yaml > temp.yaml
+	python add_prefixes_from_file.py \
+		--model_to_supplement_file temp.yaml \
+		--additional_prefixes_file prefixes_suffix.yaml > $@
 	
 personinfo/personinfo.py: personinfo.yaml
 	gen-project -d personinfo personinfo.yaml
--- add_prefixes_from_file.py
+++ add_prefixes_from_file.py
@@ -1,4 +1,5 @@
 import yaml
+import click
 
 
 def ysl_wrapper(yamlfile):
@@ -13,15 +14,34 @@ def ysl_wrapper(yamlfile):
 supplemtary_prefixes_file = "prefixes_suffix.yaml"
 deficient_linkml_file = "personinfo.yaml"
 
-prefixes_suffix = ysl_wrapper(supplemtary_prefixes_file)
 
-model_to_supplement = ysl_wrapper(deficient_linkml_file)
 
-prefixes_to_supplement = model_to_supplement["prefixes"]
+@click.command()
+# like personinfo.yaml
+@click.option('-m', '--model_to_supplement_file', type=click.Path(exists=True), required=True)
+# like 
+@click.option('-p', '--additional_prefixes_file', type=click.Path(exists=True), required=True)
+def main(model_to_supplement_file, additional_prefixes_file):
+	# print(model_to_supplement_file)
+	# print(additional_prefixes_file)
+	prefixes_suffix = ysl_wrapper(additional_prefixes_file)
+	# print(prefixes_suffix['prefixes'])
 
-prefixes_to_supplement.update(prefixes_suffix["prefixes"])
-model_to_supplement["prefixes"] = prefixes_to_supplement
+	model_to_supplement = ysl_wrapper(model_to_supplement_file)
 
-with open(deficient_linkml_file, "w") as outfile:
-    yaml.safe_dump(model_to_supplement, outfile, default_flow_style=False)
+	prefixes_to_supplement = model_to_supplement["prefixes"]
+	# print(model_to_supplement)
 
+
+	prefixes_to_supplement.update(prefixes_suffix["prefixes"])
+	model_to_supplement["prefixes"] = prefixes_to_supplement
+
+	# with open(additional_prefixes_file, "w") as outfile:
+	#     yaml.safe_dump(model_to_supplement, outfile, default_flow_style=False)
+
+	dumped = yaml.dump(model_to_supplement, default_flow_style=False, sort_keys=False)
+	print(dumped)
+
+
+if __name__=="__main__": 
+    main() 
